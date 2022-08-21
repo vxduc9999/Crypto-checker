8a2cbda52189da7d4260f6b3f5faefca53bee2b4
ducvu <vxduc9999@gmail.com>
2022-08-21 11:23:51 +0700

    remove Switch not use to deploy

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
 import Home from "./components/Home";
 import CoinDetail from "./components/CoinDetail";
-import { BrowserRouter as Router, Switch, Routes, Route } from "react-router-dom";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 
 import './App.css'
 
